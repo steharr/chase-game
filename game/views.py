0005f6e413cc24160708776b0da945588dbd6c92
Stephen Harrold <steharrold@gmail.com>
2022-03-12 16:49:31 +0000

    refactor game board setup

--- game/views.py
+++ game/views.py
@@ -5,5 +5,5 @@ def game(request):
     """Displays game board"""
     rows = 10
     cols = 10
-    context = {'rows': rows, 'cols': cols}
+    context = {'rows': range(rows, -1, -1), 'cols': range(0, cols, 1)}
     return render(request, 'game/game.html', context)
