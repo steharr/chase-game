258ae1821aed5c63a7a59554ec2c471dcc6a6487
Stephen Harrold <steharrold@gmail.com>
2022-03-14 19:28:41 +0000

    clean up route display

--- game/static/game/js/Route.js
+++ game/static/game/js/Route.js
@@ -3,11 +3,35 @@ export class Route {
         this.route = [];
     }
     record(pos) {
+        let icon;
+        if (this.route.length !== 0) {
+            let prev = this.route[this.route.length - 1];
+            let diff_x = pos[0] - prev[0];
+            let diff_y = pos[1] - prev[1];
+            if (diff_x > 0) {
+                icon = 'arrow-right';
+            } else if (diff_x < 0) {
+                icon = 'arrow-left';
+            } else if (diff_y > 0) {
+                icon = 'arrow-up';
+            } else if (diff_y < 0) {
+                icon = 'arrow-down';
+            }
+            pos.push(icon);
+        } else {
+            pos.push('circle');
+        }
         this.route.push(pos);
     }
     display() {
-        this.route.forEach(function (step, index) {
-            $(`div[data-x="${step[0]}"][data-y="${step[1]}"]`).css('background-color', 'orange').text(index);
-        });
+
+        // highlight the starting point with a circle
+        $(`div[data-x="${this.route[0][0]}"][data-y="${this.route[0][1]}"]`).addClass('route').html(`<i class="fa-solid fa-${this.route[0][2]}"></i>`);
+
+        // highlight the route taken with arrows
+        for (let i = 1; i < this.route.length; i++) {
+            $(`div[data-x="${this.route[i][0]}"][data-y="${this.route[i][1]}"]`).addClass('route').html(`<i class="fa-solid fa-${this.route[i+1][2]}"></i>`);
+        }
+
     }
 }
