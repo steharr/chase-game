2e926f9373decd4571826dc46b52512e5b43edc4
Stephen Harrold <steharrold@gmail.com>
2022-04-19 20:54:45 +0100

    add game board

--- src/main/java/chasegame/controllers/GameController.java
+++ src/main/java/chasegame/controllers/GameController.java
@@ -1,8 +1,8 @@
 package chasegame.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import chasegame.GameBoard;
@@ -11,13 +11,21 @@ import chasegame.GameBoard;
 @Controller
 public class GameController {
 
-	@ModelAttribute(name = "gameBoard")
-	public GameBoard gameBoard() {
-		return new GameBoard(10, 10);
+	private GameBoard gameBoard;
+
+	public void createGameBoard(Model model) {
+		this.gameBoard = new GameBoard(10, 10);
+
+		int[] gameRows = new int[this.gameBoard.getRows()];
+		int[] gameColumns = new int[this.gameBoard.getColumns()];
+
+		model.addAttribute("rows", gameRows);
+		model.addAttribute("columns", gameColumns);
 	}
 
 	@GetMapping
-	public String startGame() {
+	public String startGame(Model model) {
+		createGameBoard(model);
 		return "game";
 	}
 
