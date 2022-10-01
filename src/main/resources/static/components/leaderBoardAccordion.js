61cdf5fff9adffc1f5c73035e8a9aaad73afd7f8
Stephen Harrold <steharrold@gmail.com>
2022-10-01 16:22:49 +0100

    (WIP) add level map to leaderboards

--- src/main/resources/static/components/leaderBoardAccordion.js
+++ src/main/resources/static/components/leaderBoardAccordion.js
@@ -1,12 +1,40 @@
 function playAccordion(node, data) {
 
     console.table(data);
+    let score = new ScoreInfo(data.score, data.date, data.routeDirections, data.routeCoordinates, );
 
     if (!node.classList.contains('expanded-row')) {
 
-        accordionElement = document.createElement("tr");
-        accordionElement.classList.add("leaderboard-row");
-        accordionElement.innerHTML = `
+        accordionElementRouteDetails = document.createElement("tr");
+        accordionElementRouteDetails.classList.add("leaderboard-row");
+
+        levelMap = document.createElement("td");
+        levelMap.colSpan = "5";
+
+        for (let r = 0; r < 15; r++) {
+            let row = document.createElement("div");
+
+
+            for (let c = 0; c < 30; c++) {
+                let col = document.createElement("div");
+                col.dataset.x = r;
+                col.dataset.y = c;
+                col.classList.add("cell-small");
+                row.appendChild(col);
+            }
+
+            levelMap.appendChild(row);
+        }
+
+        accordionElementRouteDetails.appendChild(levelMap);
+        node.parentNode.insertBefore(accordionElementRouteDetails, node.nextSibling);
+
+
+
+
+        accordionElementScoreDetails = document.createElement("tr");
+        accordionElementScoreDetails.classList.add("leaderboard-row");
+        accordionElementScoreDetails.innerHTML = `
             <td>
                 <div class="star"><i class="fa-solid fa-star gold"></i></div>
             </td>
@@ -15,7 +43,13 @@ function playAccordion(node, data) {
             <td>data.date</td>
             <td>data.date</td>`;
 
-        node.parentNode.insertBefore(accordionElement, node.nextSibling);
+        node.parentNode.insertBefore(accordionElementScoreDetails, node.nextSibling);
+
+
+
+
+
+
 
         node.classList.add('expanded-row');
     } else {
@@ -23,5 +57,29 @@ function playAccordion(node, data) {
         node.nextSibling.remove();
 
         node.classList.remove('expanded-row');
+    }
+}
+
+class ScoreInfo {
+
+    constructor(points, date, directions, coordinates) {
+        this.points = points;
+        this.date = date;
+        this.directions = this.parseData(directions);
+        this.coordinates = this.parseData(coordinates);
+    }
+
+    parseData(data) {
+        let dataPoints = data.split("+");
+
+        if (dataPoints.length > 1 && dataPoints[1].includes(",")) {
+            let _dataPoints = new Array();
+            dataPoints.forEach(e => {
+                _dataPoints.push(e.split(","));
+            })
+            dataPoints = _dataPoints;
+        }
+
+        return dataPoints;
     }
 }
