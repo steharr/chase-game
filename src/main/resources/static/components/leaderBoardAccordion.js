5cd82091013a50c8df4543cb84e94e900be2a04e
Stephen Harrold <steharrold@gmail.com>
2022-10-05 17:23:31 +0100

    fix expanding rows and add heatmap display

--- src/main/resources/static/components/leaderBoardAccordion.js
+++ src/main/resources/static/components/leaderBoardAccordion.js
@@ -1,55 +1,81 @@
-function playAccordion(node, data) {
+function playAccordion(clickedNode, data) {
 
     let score = new ScoreInfo(data.score, data.date, data.routeDirections, data.routeCoordinates, );
     console.table(score);
 
-    if (!node.classList.contains('expanded-row')) {
+
+    if (!clickedNode.classList.contains('expanded-row')) {
+
+        clearExpandedRows();
 
         let levelMapRow = createLeaderBoardRow();
         levelMapRow.appendChild(createLevelMapDetailsContainer());
-        node.parentNode.insertBefore(levelMapRow, node.nextSibling);
+        clickedNode.parentNode.insertBefore(levelMapRow, clickedNode.nextSibling);
 
-        let scoreDetailsRow = createLeaderBoardRow();
-        scoreDetailsRow.innerHTML = createScoreDetailsContainer(score);
-        node.parentNode.insertBefore(scoreDetailsRow, node.nextSibling);
+        // let scoreDetailsRow = createLeaderBoardRow();
+        // scoreDetailsRow.innerHTML = createScoreDetailsHeaderContainer(score);
+        // clickedNode.parentNode.insertBefore(scoreDetailsRow, clickedNode.nextSibling);
 
-        node.classList.add('expanded-row');
+        clickedNode.classList.add('expanded-row');
 
-        mapScoreCoordinatesToTable(score)
+        mapScoreCoordinatesToTable(score);
 
-    } else {
 
-        node.nextSibling.remove();
-        node.classList.remove('expanded-row');
+
+    } else if (clickedNode.classList.contains('expanded-row')) {
+
+        clickedNode.nextSibling.remove();
+        clickedNode.nextSibling.remove();
+        clickedNode.nextSibling.remove();
+        clickedNode.classList.remove('expanded-row');
+
     }
+
+}
+
+function clearExpandedRows() {
+    const rows = document.getElementsByClassName('expanded-row');
+    Array.from(rows).forEach(r => {
+        r.classList.remove('expanded-row');
+    });
+
+    const detailRows = document.getElementsByClassName('leaderboard-row-detail');
+    Array.from(detailRows).forEach(r => {
+        r.remove();
+    });
+
 }
 
 function mapScoreCoordinatesToTable(score) {
 
-    score.coordinates.forEach(c => {
+    const heatMapGradient = gradient('#c9eb34', '#eb3434', score.coordinates.length);
 
-        const cell = document.querySelector(`td[data-x="${c[0]}"][data-y="${c[1]}"]`);
+    score.coordinates.forEach((coordinate, index) => {
+
+        let cell = document.querySelector(`td[data-x="${coordinate[0]}"][data-y="${coordinate[1]}"]`);
 
         if (null !== cell) {
-            cell.classList.add('cell-small-filled');
+            cell.style.backgroundColor = String(heatMapGradient[index]);
         }
 
     });
+
+
 }
 
 function createLeaderBoardRow() {
     row = document.createElement("tr");
-    row.setAttribute('id', 'test');
-    row.classList.add("leaderboard-row");
+    row.classList.add("leaderboard-row-detail");
     return row;
 }
 
 function createLevelMapDetailsContainer() {
 
-    levelMapContainer = document.createElement("td")
+    levelMapContainer = document.createElement("td");
     levelMapContainer.colSpan = "5";
 
     levelMapTable = document.createElement("table");
+    levelMapTable.classList.add('level-map-table');
     levelMapContainer.appendChild(levelMapTable);
 
     for (let r = 0; r < 15; r++) {
@@ -61,7 +87,7 @@ function createLevelMapDetailsContainer() {
             col.dataset.x = r;
             col.dataset.y = c;
             col.classList.add("cell-small");
-            row.appendChild(col);
+            row.prepend(col);
         }
 
         levelMapTable.appendChild(row);
@@ -70,16 +96,13 @@ function createLevelMapDetailsContainer() {
     return levelMapContainer;
 }
 
-function createScoreDetailsContainer(score) {
+function createScoreDetailsHeaderContainer(score) {
 
     return `
-        <td>
-            <div class="star"><i class="fa-solid fa-star gold"></i></div>
+        <td colspan="5">
+            <div class="text-center">Heat Map</div>
         </td>
-        <td></td>
-        <td>data.score</td>
-        <td>data.date</td>
-        <td>data.date</td>`;
+        `;
 
 }
 
@@ -105,4 +128,37 @@ class ScoreInfo {
 
         return dataPoints;
     }
+}
+
+// *** https://stackoverflow.com/questions/12934720/how-to-increment-decrement-hex-color-values-with-javascript-jquery ***
+function gradient(startColor, endColor, steps) {
+    var start = {
+        'Hex': startColor,
+        'R': parseInt(startColor.slice(1, 3), 16),
+        'G': parseInt(startColor.slice(3, 5), 16),
+        'B': parseInt(startColor.slice(5, 7), 16)
+    }
+    var end = {
+        'Hex': endColor,
+        'R': parseInt(endColor.slice(1, 3), 16),
+        'G': parseInt(endColor.slice(3, 5), 16),
+        'B': parseInt(endColor.slice(5, 7), 16)
+    }
+    diffR = end['R'] - start['R'];
+    diffG = end['G'] - start['G'];
+    diffB = end['B'] - start['B'];
+
+    stepsHex = new Array();
+    stepsR = new Array();
+    stepsG = new Array();
+    stepsB = new Array();
+
+    for (var i = 0; i <= steps; i++) {
+        stepsR[i] = start['R'] + ((diffR / steps) * i);
+        stepsG[i] = start['G'] + ((diffG / steps) * i);
+        stepsB[i] = start['B'] + ((diffB / steps) * i);
+        stepsHex[i] = '#' + Math.round(stepsR[i]).toString(16) + '' + Math.round(stepsG[i]).toString(16) + '' + Math.round(stepsB[i]).toString(16);
+    }
+    return stepsHex;
+
 }
