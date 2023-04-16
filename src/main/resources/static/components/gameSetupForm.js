20af37fcb0ab65d55111739c64a72292ac2353eb
steharr <steharrold@gmail.com>
2023-04-16 01:00:59 +0100

    update ui

--- src/main/resources/static/components/gameSetupForm.js
+++ src/main/resources/static/components/gameSetupForm.js
@@ -15,11 +15,17 @@ function postGameSetup() {
         obstacles: chosenObstacles,
     };
 
-    fetch(postUrl, {
-        method: "POST",
-        headers: getHttpHeaders(),
-        body: JSON.stringify(data)
-    }).then(res => console.log(res)).catch(err => console.log(err));
+    const spinner = new Spinner("postSetup");
+    spinner.startSpinner();
+    setTimeout(()=>{
+        fetch(postUrl, {
+            method: "POST",
+            headers: getHttpHeaders(),
+            body: JSON.stringify(data)
+        }).then(res => {
+            spinner.stopSpinner("Saved")
+        }).catch(err => console.log(err));
+    }, 500);
 
 };
 
@@ -31,4 +37,37 @@ function getHttpHeaders() {
         "charset": "UTF-8",
         "Content-Type": "application/json"
     }
+}
+
+ class Spinner {
+    constructor(elementId){
+        this.id = elementId;
+        this.spinnerDisplayed = false;
+        this.element = document.getElementById(this.id);
+        this.SPINNER_HTML = `
+        <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+        </div>
+        `
+    }
+
+    startSpinner(){
+        this.element.innerHTML = this.SPINNER_HTML;
+        this.spinnerDisplayed = true;
+    }
+
+    stopSpinner(message="", disableBtn=false){
+        this.element.innerHTML = message;
+        this.spinnerDisplayed = false;
+        if (disableBtn){
+            this.element.disabled = true;
+        }
+    }
+
+    toggleSpinner(){
+        this.spinnerDisplayed ? this.stopSpinner(): this.startSpinner();
+        this.spinnerDisplayed = !this.spinnerDisplayed;
+    }
+
+
 }
